a7ccb8bf1b66b2df0cc24d76cf8d3e8f10df7c30
WordPressFan <ahmed@wp-media.me>
2025-07-08 09:38:12 +0300

    remove not needed changes

--- src/PostInstall.php
+++ src/PostInstall.php
@@ -12,8 +12,8 @@ class PostInstall {
      * @var array
      */
     private static $files = [
-        'PluginFamily.php',
-        'wp_media_plugins.php',
+        'PluginFamily',
+        'wp_media_plugins',
     ];
 
     /**
