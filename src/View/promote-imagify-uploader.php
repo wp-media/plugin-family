66e0c0c40129d01cb3fdf722ca8d9a412f6c6a18
prades <pratik.d@rankmath.com>
2025-10-15 14:09:34 +0530

    Added the condition to check if the $notice is set

--- src/View/promote-imagify-uploader.php
+++ src/View/promote-imagify-uploader.php
@@ -8,9 +8,11 @@ defined( 'ABSPATH' ) || exit;
 				<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
 			</svg>
 		</button>
-		<p>
-			<?php echo esc_html( $notice ); ?>
-		</p>
+		<?php if ( ! empty( $notice ) ) { ?>
+			<p>
+				<?php echo esc_html( $notice ); ?>
+			</p>
+		<?php } ?>
 		<button id="pluginfamily_install_imagify"><?php esc_html_e( 'Install Imagify Plugin', '%domain%' ); ?></button>
 	</div>
 </script>
